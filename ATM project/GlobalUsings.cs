a2303721c73dc190cf67eba1acccc0e44fc16f2c
JamesLizamore <jameswlizamore@gmail.com>
2024-07-22 17:02:54 +0200

    Created view method for accounts

---
+++ ATM project/GlobalUsings.cs
@@ -0,0 +1,6 @@
+// Global using directives
+
+global using System.Collections;
+global using System.Data;
+global using System.Data.SqlClient;
+global using System.Globalization;
---
+++ ATM project/Program.cs
@@ -0,0 +1,126 @@
+﻿namespace ATM_project;
+
+class Program
+{
+    
+    static string connectionString = @"Server=(localdb)\MSSQLLocalDB";
+    static void Main(string[] args)
+        {
+            // viewAccounts();
+            
+            viewTransaction();
+            
+
+
+        }
+        static void logIn()
+        {
+
+        }
+
+        static void mainMenu()
+        {
+
+        }
+
+        static void viewAccounts()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var selectAllAccounts = connection.CreateCommand();
+                selectAllAccounts.CommandText = $"SELECT * FROM TopazBanking.dbo.Accounts";
+                List<Account> tableData = new();
+                SqlDataReader reader = selectAllAccounts.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        tableData.Add(
+                            new Account
+                            {
+                                accountID = reader.GetString(0),
+                                userID = reader.GetString(1),
+                                accountNumber = reader.GetString(2),
+                                balance = reader.GetDecimal(3),
+                            });
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No accounts found");
+                }
+                connection.Close();
+
+                foreach (var account in tableData)
+                {
+                    Console.WriteLine($"{account.userID} {account.accountID} {account.accountNumber} {account.balance:c}");
+                }
+            }
+        }
+
+        static void viewTransaction()
+        {
+            using (var trans = new SqlConnection(connectionString))
+            {
+                trans.Open();
+                var selectAllTransaction = trans.CreateCommand();
+                selectAllTransaction.CommandText = $"SELECT * FROM TopazBanking.dbo.Transactions";
+                List<Transaction> tableTrans = new();
+                SqlDataReader reader = selectAllTransaction.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        tableTrans.Add(
+                            new Transaction
+                            {
+                                transactionID = reader.GetString(0),
+                                accountID = reader.GetString(1),
+                                transactionType = reader.GetString(2),
+                                amount = reader.GetDecimal(3),
+                                transactionDateTime = reader.GetDateTime(4),
+                            });
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No transactions found");
+                }
+                trans.Close();
+                
+                foreach (var transaction in tableTrans)
+                {
+                    Console.WriteLine($"{transaction.transactionID} {transaction.accountID} {transaction.transactionType} {transaction.amount:c} {transaction.transactionDateTime}");
+                }
+            }
+        }
+
+        public class Account
+        {
+            public string accountID { get; set; }
+            public string userID { get; set; }
+            public string accountNumber { get; set; }
+            public decimal balance { get; set; }
+        }
+
+        public class Users
+        {
+            public string userID { get; set; }
+            public string userName { get; set; }
+            public string userPIN { get; set; }
+        }
+
+        public class Transaction
+        {
+            //AccountID for transactions
+
+            public string transactionID { get; set; }
+            public string accountID { get; set; }
+            public string transactionType { get; set; }
+            public decimal amount { get; set; }
+            public DateTime transactionDateTime { get; set; }
+        }
+}
